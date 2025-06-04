a8d02062ae4ebd07d058c74c2947c2acd3dbcc4f
tamer ugur <tamerugur3@hotmail.com>
2025-06-04 13:42:43 +0300

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@shared': path.resolve(__dirname, '../shared'),
+    },
+  },
   server: {
     proxy: {
-      '/api': 'http://localhost:3001'
-    }
-  }
+      '/api': 'http://localhost:3001',
+    },
+  },
 })
