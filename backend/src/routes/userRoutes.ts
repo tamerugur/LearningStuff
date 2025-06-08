b1cc46b31ae01c699bf16934963a6d5b07a68e84
tamerugur <tamerugurr@gmail.com>
2025-06-08 22:02:10 +0300

    decided to use microservice architecture

--- backend/src/routes/userRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { checkUnique, registerUser } from '../controllers/userController'
-
-const router = express.Router()
-
-router.post('/register', registerUser)
-router.post('/check-unique', checkUnique)
-
-export default router
