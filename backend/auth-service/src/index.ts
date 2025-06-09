fa3ac7771f909f5008b80ad796cc3256ed98747b
tamerugur <tamerugur3@hotmail.com>
2025-06-09 18:02:12 +0300

    created basic userservice

--- backend/auth-service/src/index.ts
+++ backend/auth-service/src/index.ts
@@ -2,18 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes";
 import cookieParser from "cookie-parser";
-import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
-const corsOptions = {
-  origin: "http://localhost:5173",
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
--- backend/user-service/src/index.ts
+++ backend/user-service/src/index.ts
@@ -1,18 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes";
-import cors from "cors";
 
 dotenv.config();
-
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
 
-const PORT = process.env.USER_SERVICE_PORT || 4002;
+const PORT = process.env.USER_SERVICE_PORT || 4001;
 app.listen(PORT, () => {
   console.log(`✅ User service running on http://localhost:${PORT}`);
 });
