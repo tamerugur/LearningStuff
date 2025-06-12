b76fa8613542cf581c9dce634949225684d90033
tamer ugur <tamerugur3@hotmail.com>
2025-06-12 17:05:11 +0300

    debugging logs and dockerized gateway

--- gateway/src/index.ts
+++ gateway/src/index.ts
@@ -1,13 +1,13 @@
-import * as express from "express";
-import * as proxy from "express-http-proxy";
-import * as cors from "cors";
-import * as dotenv from "dotenv";
+import express from "express";
+import proxy from "express-http-proxy";
+import cors from "cors";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
+const PORT = process.env.PORT || 4000;
+app.use(express.json());
 app.use(
   cors({
     origin: process.env.FRONTEND_ORIGIN || "http://localhost:5173",
@@ -17,27 +17,63 @@ app.use(
   })
 );
 
+
+app.use(
+  (
+    err: Error,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    console.error("🔥 Unhandled error in gateway:", err);
+    res.status(500).json({ error: "Gateway internal error" });
+  }
+);
+
+app.use((req, res, next) => {
+  console.log(`[Gateway] ${req.method} ${req.originalUrl}`);
+  next();
+});
+
 app.use(
   "/api/auth",
-  proxy(process.env.AUTH_SERVICE_URL || "http://localhost:4001", {
-    proxyReqPathResolver: (req) => {
-      return req.originalUrl;
+  proxy(process.env.AUTH_SERVICE_URL || "http://auth-service:4001", {
+    proxyReqPathResolver: (req) => req.originalUrl,
+
+    proxyReqBodyDecorator: (body, req) => {
+      return body;
+    },
+
+    proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+      return {
+        ...proxyReqOpts,
+        headers: {
+          ...proxyReqOpts.headers,
+          "Content-Type": "application/json",
+        },
+      };
+    },
+
+    proxyErrorHandler: (err, res, next) => {
+      console.log("❌ Proxy to auth-service failed:", err.message);
+      res
+        .status(500)
+        .json({ error: "Auth serviceasndasdkjsdjklasd unavailable" });
     },
   })
 );
 
+// Proxy to User Service
 app.use(
   "/api/users",
-  proxy(process.env.USER_SERVICE_URL || "http://localhost:4002", {
-    proxyReqPathResolver: (req) => {
-      return req.originalUrl;
+  proxy(process.env.USER_SERVICE_URL || "http://user-service:4002", {
+    proxyReqPathResolver: (req) => req.originalUrl,
+    proxyErrorHandler: (err, res, next) => {
+      console.error("❌ Proxy to user-service failed:", err.message);
+      res.status(500).json({ error: "User service unavailable" });
     },
   })
 );
-
-app.get("/", (req, res) => {
-  res.send("Gateway is running");
-});
 
 app.listen(PORT, () => {
   console.log(`✅ Gateway listening on port ${PORT}`);
