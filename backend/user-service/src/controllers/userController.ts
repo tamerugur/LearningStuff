34ee28c6459153ce1c497cac91ee61bd12750f8c
tamer ugur <tamerugur3@hotmail.com>
2025-06-12 14:24:39 +0300

    commented out for now

--- backend/user-service/src/controllers/userController.ts
+++ backend/user-service/src/controllers/userController.ts
@@ -3,18 +3,18 @@ import { UserService } from "../services/userService";
 import { AuthRequest } from "../middleware/authMiddleware";
 
 export async function getProfile(req: AuthRequest, res: Response) {
-  try {
-    const userPayload = req.user;
-    if (!userPayload) {
-      return res.status(401).json({ error: "Unauthorized" });
-    }
+  // try {
+  //   const userPayload = req.user;
+  //   if (!userPayload) {
+  //     return res.status(401).json({ error: "Unauthorized" });
+  //   }
 
-    const user = await UserService.findOrCreateUser(userPayload);
+  //   const user = await UserService.findOrCreateUser(userPayload);
 
-    res
-      .status(200)
-      .json({ message: `Welcome to the main page, ${user.fullName}!` });
-  } catch (err: any) {
-    res.status(500).json({ error: "An unexpected error occurred" });
-  }
+  //   res
+  //     .status(200)
+  //     .json({ message: `Welcome to the main page, ${user.fullName}!` });
+  // } catch (err: any) {
+  //   res.status(500).json({ error: "An unexpected error occurred" });
+  // }
 }
--- backend/user-service/src/services/userService.ts
+++ backend/user-service/src/services/userService.ts
@@ -10,23 +10,23 @@ interface UserPayload {
 
 export const UserService = {
   async findOrCreateUser(payload: UserPayload) {
-    const { id, email, fullName } = payload;
+    // const { id, email, fullName } = payload;
 
-    let user = await prisma.user.findUnique({
-      where: { id },
-    });
+    // let user = await prisma.user.findUnique({
+    //   where: { id },
+    // });
 
-    if (!user) {
-      // User doesn't exist in this service's DB, create them.
-      user = await prisma.user.create({
-        data: {
-          id,
-          email,
-          fullName,
-        },
-      });
-    }
+    // if (!user) {
+    //   // User doesn't exist in this service's DB, create them.
+    //   user = await prisma.user.create({
+    //     data: {
+    //       id,
+    //       email,
+    //       fullName,
+    //     },
+    //   });
+    // }
 
-    return user;
+    // return user;
   },
 };
