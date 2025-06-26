eea739f0ecffbdc16453c04c6327b755dbe7f250
tamer ugur <tamerugur3@hotmail.com>
2025-06-26 16:29:35 +0300

    rabbitmq setup

--- backend/auth-service/src/index.ts
+++ backend/auth-service/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes";
 import cookieParser from "cookie-parser";
+import { setupRabbitMQ } from './events/rabbitmq';
 
 dotenv.config();
 console.log("DATABASE_URL from .env:", process.env.DATABASE_URL);
@@ -10,10 +11,22 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-
 app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 4001;
-app.listen(PORT, () => {
-  console.log(`✅ Auth service running on http://localhost:${PORT}`);
-});
+
+async function bootstrap() {
+  try {
+    await setupRabbitMQ();
+    console.log("📡 RabbitMQ initialized");
+
+    app.listen(PORT, () => {
+      console.log(`✅ Auth service running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("❌ Failed to initialize RabbitMQ:", error);
+    process.exit(1);
+  }
+}
+
+bootstrap();
