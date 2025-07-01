9cb77804c340e3ea7f747f5e504dd36887597738
tamer ugur <tamerugur3@hotmail.com>
2025-07-01 10:05:33 +0300

    starting rabbitmq from scratch

--- backend/auth-service/src/events/publishUserCreated.ts
+++
@@ -1,6 +0,0 @@
-import { UserCreatedEvent, USER_EVENTS } from '../../../event-schemas/userEvents';
-import { publishToExchange } from './rabbitmq';
-
-export function publishUserCreatedEvent(payload: UserCreatedEvent) {
-  publishToExchange(USER_EVENTS.CREATED, payload);
-}
